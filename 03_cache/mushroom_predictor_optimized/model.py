8cad0e5eecdc69b1bbb7fbe685c957635b9af635
Matheus Henrique Fonseca Afonso <henrique.matheus@gec.inatel.br>
2025-02-17 18:36:12 -0300

    feat(project): Model trained
    A model dedicated to
    classify the mushrooms
    was created. The idea
    is to load te model, so
    the interface will run
    faster.

---
+++ 03_cache/mushroom_predictor_optimized/model.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from joblib import dump
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.ensemble import GradientBoostingClassifier
+
+DATA = "03_cache\mushroom_predictor_optimized\data\mushrooms.csv"
+COLS = [
+    "class",
+    "odor",
+    "gill-size",
+    "gill-color",
+    "stalk-surface-above-ring",
+    "stalk-surface-below-ring",
+    "stalk-color-above-ring",
+    "stalk-color-below-ring",
+    "ring-type",
+    "spore-print-color",
+]
+
+# Read data
+df = pd.read_csv(DATA)
+df = df[COLS]
+
+# Create pipeline
+pipe = Pipeline(
+    [
+        ("encoder", OrdinalEncoder()),
+        ("gbc", GradientBoostingClassifier(max_depth=5, random_state=42)),
+    ]
+)
+
+# Fit the pipeline
+X = df.drop(["class"], axis=1)
+y = df["class"]
+
+pipe.fit(X, y)
+
+# Save the pipeline
+dump(pipe, "03_cache\mushroom_predictor_optimized\model\pipe.joblib")
