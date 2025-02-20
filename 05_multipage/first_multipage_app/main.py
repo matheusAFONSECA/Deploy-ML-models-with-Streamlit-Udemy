935eecba379f5bda076722d0cbf5eb92e9b5b7fc
Matheus Henrique Fonseca Afonso <henrique.matheus@gec.inatel.br>
2025-02-20 11:32:57 -0300

    fix(project): Better communication in pages
    Now the widgets have a better
    communication between pages.

--- 05_multipage/first_multipage_app/main.py
+++ 05_multipage/first_multipage_app/main.py
@@ -1,39 +1,51 @@
 import streamlit as st
-import pandas as pd
 
 st.set_page_config(
     page_title="Homepage",
     page_icon="🏡",
     layout="centered",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded",
 )
 
-df = pd.DataFrame({"col1": [1, 2, 3],
-                   "col2": [4, 5, 6]})
-
-if "df" not in st.session_state:
-    st.session_state['df'] = df
-
-if "product" not in st.session_state:
-    st.session_state['product'] = 0
+if all(key not in st.session_state.keys() for key in ("product", "x1", "x2")):
+    st.session_state["x1"] = 0
+    st.session_state["x2"] = 0
+    st.session_state["product"] = 0
 
 
 def multiply(x1, x2):
     st.session_state["product"] = x1 * x2
 
 
-if __name__ == "__main__":
+# Workaround from: https://stackoverflow.com/questions/74968179/session-state-is-reset-in-streamlit-multipage-app
 
+
+def keep(key):
+    # Copy from temporary widget key to permanent key
+    st.session_state[key] = st.session_state[f"_{key}"]
+
+
+def unkeep(key):
+    # Copy from permanent key to temporary widget key
+    st.session_state[f"_{key}"] = st.session_state[key]
+
+
+if __name__ == "__main__":
     st.title("Homepage")
 
     col1, col2 = st.columns(2)
 
     with col1:
-        x1 = st.number_input("Pick a number", 0, 10, key="x1")
+        unkeep("x1")
+        x1 = st.number_input(
+            "Pick a number", 0, 10, key="_x1", on_change=keep, args=(("x1",))
+        )
     with col2:
-        x2 = st.number_input("Pick another number", 0, 10, key='x2')
+        unkeep("x2")
+        x2 = st.number_input(
+            "Pick another number", 0, 10, key="_x2", on_change=keep, args=(("x2",))
+        )
 
     st.button("Multiply!", type="primary", on_click=multiply, args=((x1, x2)))
 
-    st.write(st.session_state['df'])
-    st.write(st.session_state)
+st.write(st.session_state)
--- 05_multipage/first_multipage_app/pages/1_page_1.py
+++ 05_multipage/first_multipage_app/pages/1_page_1.py
@@ -2,10 +2,8 @@ import streamlit as st
 
 st.title("First page")
 
-st.write(st.session_state['df'])
-
-x1 = st.session_state['x1']
-x2 = st.session_state['x2']
+x1 = st.session_state["x1"]
+x2 = st.session_state["x2"]
 
 st.subheader(f"You chose to multiply {x1} with {x2} 👍")
 st.markdown("""#### Check the second page for the result!""")
